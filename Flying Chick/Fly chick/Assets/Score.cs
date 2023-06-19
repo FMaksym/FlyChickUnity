808d916216f383e388609252e77292ed46bc80fc
QuakeOut <87473664+FMaksym@users.noreply.github.com>
2023-06-19 13:08:16 +0300

    Update Score.cs

--- Flying Chick/Fly chick/Assets/Score.cs
+++ Flying Chick/Fly chick/Assets/Score.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class Score : MonoBehaviour
 {
     public static int score = 0;
-    // Start is called before the first frame update
+
     private void Start()
     {
         score = 0;
     }
 
-    // Update is called once per frame
     private void Update()
     {
         GetComponent < UnityEngine.UI.Text>().text = score.ToString();
